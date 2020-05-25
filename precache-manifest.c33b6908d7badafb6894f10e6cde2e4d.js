9e97d82413f846f08943c6e6eb5772adcaaff342
hacker-mu <hacker-mu@uvcat.com>
2020-05-25 14:00:30 +0800

    deploy

---
+++ precache-manifest.c33b6908d7badafb6894f10e6cde2e4d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e767c166cb0564db20bd",
+    "url": "/day-progress/css/app.211f4755.css"
+  },
+  {
+    "revision": "e034df8ef1322cec5e89",
+    "url": "/day-progress/css/chunk-vendors.fb0f6057.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/day-progress/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "184487e83f128b3c8a7c382706044237",
+    "url": "/day-progress/index.html"
+  },
+  {
+    "revision": "8ae94b7fc650d13f9057",
+    "url": "/day-progress/js/about.164c4188.js"
+  },
+  {
+    "revision": "e767c166cb0564db20bd",
+    "url": "/day-progress/js/app.41bc1836.js"
+  },
+  {
+    "revision": "e034df8ef1322cec5e89",
+    "url": "/day-progress/js/chunk-vendors.c1605696.js"
+  },
+  {
+    "revision": "294ee18ee4e99b47dab9a140422f5821",
+    "url": "/day-progress/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/day-progress/robots.txt"
+  }
+]);
